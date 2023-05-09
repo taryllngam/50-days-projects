0359141b75274f0a9752ac2b68f8657e73e4e9e5
NGAM TARYLL <taryllngam@gmail.com>
2023-05-09 16:11:31 +0100

    added fucntionality to the project

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg') 
+const bg = document.querySelector('.bg')
 
+let load = 0
+const int = setInterval(blurring, 30)
 
-let load = 0;
-let int = setInterval(blurring, 30)
+function blurring () {
+  load++
 
-function blurring() {
-     load++
+  if (load > 99) {
+    clearInterval(int)
+  }
+  console.log(load)
 
-     if(load > 99){
-       clearInterval(int)
-     }
-     console.log(load)
-
-     loadText.innerText = `${load}%`
-     loadText.style.opacity =  scale(load, 0, 100, 1, 0)
-     bg.style.filter = `blur(${scale(load,0,  100, 30, 0)}px)`
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 function scale (number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
 }
